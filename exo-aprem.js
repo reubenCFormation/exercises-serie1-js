002635f8b7890ef9b45017f29869e9f23113dd5d
yunities-front <reubenCFormation@gmail.com>
2023-05-01 12:08:52 +0200

    exo-serie1

---
+++ exo-aprem.js
@@ -0,0 +1,21 @@
+//1) A partir de ce tableau, trouver un moyen d'afficher la somme de la longeur de chaque element du tableau et stocker ce resultat en une variable. Faites un console.log de ce resultat
+var string=["first string","second string","third string"];
+
+//2) A partir de ce tableau d'objets pour chaque element du tableau, afficher le type de chaque objet dans le tableau
+var typesArr=[{value:"Hello, I am a string"},{value:false},{value:25},{value:30}];
+
+//3) A partir de ce tableau d'objets ecrivez (avec une concatenation) en une seul chaine de caracteres, le type et le nom du premier et avant dernier element du tableau (Pour l'avant dernier element du tableau, utilisez une valuer dynamique!) du genre "le premier element du tableau et de type.... et on l'appelle un....."
+var animals=[{type:"Mammal",name:"Elephant"},{type:"Mammal",name:"Cat"},{type:"Mammal",name:"Dog"},{type:"Reptile",name:"Crocodile"},{type:"Reptile",name:"Lizzard"}];
+
+//4) Rajouter un element au tableau, et faites la meme chose, mais cette fois, de maniere dynamique (comme le bonus de l'exercise d'avant)
+
+//5) A partir de ces deux tableaux, verifiez pour chaque element du 2eme tableau, si il est contenu dans le premier tableau.
+// Bonus (pour ceux qui veulent decouvrir les conditions!): Stockez en une variable le nombre d'elements dans le 2eme tableau qui ne sont pas contenu dans le premier tableau 
+var arr1=["Dog","Lizard","Cat","Mouse","Elephant"];
+var arr2=["Dog","Rat","Tiger","Wolf","Elephant","Frog"];
+
+
+//6) A partir de cette chaine de caractere, créer un tableau pour chaque virgule. (Il va falloir chercher pour voir comment transformer une chaine de caracter en tableau). Apres retransformer le tableau un chaine de caraecter mais cette fois si, sans virgules!
+var myString="This,will,be,an,array,seperated by,a comma"
+
+
---
+++ exo-matin.js
@@ -0,0 +1,12 @@
+//1) J'ai un tableau avec 5 personnes. Afficher la premiere valeur et la derniere valeur de ce tableau. stocker les resultats dans deux variables et faites un console.log de ces deux variable
+ var persons=["Pierre","Elodie","Virginie","Vincent","Nicolas"];
+ 
+
+ //2) J'ai un tableau de numeros. Afficher la somme du 2eme et avant dernier element du tableau. Stocker le resultat dans une variable et faites un console.log de cette variable 
+ var numbers=[10,20,30,40,50,60,65];
+
+ //3) J'ai un tableau d'objets d'etudiants. Affiche la moyenne des notes ainsi que la moyenne des ages dans deux variables differentes et  faites un console.log de ces variables
+ var students=[{name:"Pierre",score:15,age:20},{name:"Alicia",score:17,age:21},{name:"Brice",score:16,age:22},{name:"Geraldine",score:18,age:25}];
+
+ //4) Bonus: Rajouter un autre etudiant au tableau (avec une methode javascript pour cela) et afficher les informations de ce dernier etudiant en affichant la valeur du dernier element de maniere dynamique! cette a dire PAS avec un students[4] (si notre tableau contient 5 elements)
+ 
