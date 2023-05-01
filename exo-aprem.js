7d34c6e58ca84b6b67f2d0f3602775873ba9e178
yunities-front <reubenCFormation@gmail.com>
2023-05-01 12:14:26 +0200

    modify exo aprem

--- exo-aprem.js
+++ exo-aprem.js
@@ -4,13 +4,13 @@ var string=["first string","second string","third string"];
 //2) A partir de ce tableau d'objets pour chaque element du tableau, afficher le type de chaque objet dans le tableau
 var typesArr=[{value:"Hello, I am a string"},{value:false},{value:25},{value:30}];
 
-//3) A partir de ce tableau d'objets ecrivez (avec une concatenation) en une seul chaine de caracteres, le type et le nom du premier et avant dernier element du tableau (Pour l'avant dernier element du tableau, utilisez une valuer dynamique!) du genre "le premier element du tableau et de type.... et on l'appelle un....."
+//3) A partir de ce tableau d'objets ecrivez (avec une concatenation) en une seul chaine de caracteres, le type et le nom du premier et avant dernier element du tableau du genre "le premier element du tableau et de type.... et on l'appelle un....."
 var animals=[{type:"Mammal",name:"Elephant"},{type:"Mammal",name:"Cat"},{type:"Mammal",name:"Dog"},{type:"Reptile",name:"Crocodile"},{type:"Reptile",name:"Lizzard"}];
 
 //4) Rajouter un element au tableau, et faites la meme chose, mais cette fois, de maniere dynamique (comme le bonus de l'exercise d'avant)
 
 //5) A partir de ces deux tableaux, verifiez pour chaque element du 2eme tableau, si il est contenu dans le premier tableau.
-// Bonus (pour ceux qui veulent decouvrir les conditions!): Stockez en une variable le nombre d'elements dans le 2eme tableau qui ne sont pas contenu dans le premier tableau 
+// Bonus (pour ceux qui veulent decouvrir les conditions et les boucles!): Stockez en une variable le nombre d'elements dans le 2eme tableau qui ne sont pas contenu dans le premier tableau 
 var arr1=["Dog","Lizard","Cat","Mouse","Elephant"];
 var arr2=["Dog","Rat","Tiger","Wolf","Elephant","Frog"];
 
